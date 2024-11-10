65335e72d3f4eff9713de3f29f89551dd559a4d6
AnwarKalonov <121517072+AnwarKalonov@users.noreply.github.com>
2024-11-09 17:06:19 -0800

    Code for turning correctly
    The code allows the car to turn only when it's in movement

--- Assets/Drive.cs
+++ Assets/Drive.cs
@@ -5,10 +5,9 @@ using UnityEngine;
 public class Drive : MonoBehaviour
 {
     public float speed;
-   
     public float topSpeed;
     public float turnSpeed;
-    public float gEarth = 9.8f;  //gravity on the earth.
+    public float gEarth = 9.8f;  // Gravity on the earth.
     Rigidbody rb;
 
     private void Start()
@@ -16,27 +15,24 @@ public class Drive : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
-
     void FixedUpdate()
     {
         rb.AddForce(new Vector3(0, -1.0f, 0) * rb.mass * gEarth);
 
-
         if (rb.GetComponent<Rigidbody>().velocity.magnitude > topSpeed)
         {
-            rb.GetComponent<Rigidbody>().velocity += transform.forward * (speed/10f) * -Input.GetAxis("Vertical");
-
-
+            rb.GetComponent<Rigidbody>().velocity += transform.forward * (speed / 10f) * -Input.GetAxis("Vertical");
         }
         else
         {
-            rb.GetComponent<Rigidbody>().velocity = transform.forward * speed * Input.GetAxis("Vertical") + (new Vector3(rb.velocity.x * 0.999f, rb.velocity.y, rb.velocity.z * 0.999f));
+            rb.GetComponent<Rigidbody>().velocity = transform.forward * speed * Input.GetAxis("Vertical") +
+                          (new Vector3(rb.velocity.x * 0.999f, rb.velocity.y, rb.velocity.z * 0.999f));
         }
 
-
-        transform.eulerAngles += new Vector3(0, turnSpeed * Input.GetAxis("Horizontal"), 0);
-
-
         
+        if (rb.velocity.magnitude > 0.5f) 
+        {
+            transform.eulerAngles += new Vector3(0, turnSpeed * Input.GetAxis("Horizontal"), 0);
+        }
     }
 }
