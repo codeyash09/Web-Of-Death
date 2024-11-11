df7c82d7f1bc84df2d36b6d983533e4bcbf1947b
codeyash09 <112034820+codeyash09@users.noreply.github.com>
2024-11-11 11:22:50 -0800

    GunSpinning and Aiming
    The gun spins and also aims.

--- Assets/Scripts/GunScript.cs
+++ Assets/Scripts/GunScript.cs
@@ -25,8 +25,32 @@ public class GunScript : MonoBehaviour
     }
 
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
+        Vector3 direction = cursor.position - transform.position;
+
+        
+
+        if (direction != Vector3.zero)
+        {
+            Quaternion rotation = Quaternion.LookRotation(direction);
+            transform.rotation = rotation;
+            transform.rotation = Quaternion.Euler(0, transform.eulerAngles.y + 90f, 0);
+
+
+            transform.GetChild(0).transform.GetChild(2).transform.localRotation = Quaternion.Euler(0, 0, rotation.eulerAngles.x);
+            if(transform.GetChild(0).GetChild(2).transform.localRotation.eulerAngles.z > 30f)
+            {
+                transform.GetChild(0).GetChild(2).transform.localRotation = Quaternion.Euler(0, 0, 30f);
+            }
+        }
+
+
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            transform.GetChild(0).GetChild(2).GetChild(0).transform.Rotate(0, 0, 30f);
+        }
+        
         
     }
 }
